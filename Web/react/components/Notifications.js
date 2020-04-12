99a0abb8df910dd2a39a51511ad90482e036371b
Veera <vssubbia@amazon.com>
2020-04-13 02:12:16 +0530

    adding pagination and rendering only 100 elements

--- Web/react/components/Notifications.js
+++ Web/react/components/Notifications.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Pagination } from 'react-bootstrap';
 
 class Notifications extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      pages: {
+        active: 1,
+        total: 1,
+        limit: 100
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.updateTotalPages(this.props.messages.length);
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.messages.length !== this.props.messages.length) {
+      this.updateTotalPages(this.props.messages.length);
+    }
+  }
+
+  updateTotalPages(length) {
+    let pages = this.state.pages;
+    pages["total"] = parseInt(length / pages.limit) + 1;
+    this.setState({ pages })
+  }
+
+  pageChanged(e) {
+    let pageItem = e.target.innerText.split("\n")[0];
+    let pages = this.state.pages;
+    let pageNumber = pages.active;
+
+    if(pageItem === "«") {
+      pageNumber = 1;
+    } else if(pageItem === "‹") {
+      pageNumber = Math.max(1, pages.active - 1);
+    } else if(pageItem === "›") {
+      pageNumber = Math.min(pages.total, pages.active + 1);
+    } else if(pageItem === "»") {
+      pageNumber = pages.total;
+    } else if(pageItem === "…") {
+    } else {
+      pageNumber = parseInt(pageItem);
+    }
+
+    if(pageNumber != NaN) {
+      pages["active"] = pageNumber;
+      this.setState({ pages: pages });
+      this.forceUpdate();
+    }
   }
 
   renderNotifications() {
     let active = this.props.filtered.source;
+    let page = this.state.pages
     return (
       (this.props.messages || [])
-      .slice(0)
-      .reverse()
       .filter((item) => (item.source === active || active === "_all"))
+      .reverse()
+      .slice((page.active - 1) * page.limit, (page.active) * page.limit)
       .map((item, index) => {
         return (
           <tr
             style={{"fontSize": "smaller"}}
             key={"notif_" + index}>
-            <td>{index}</td>
+            <td>{index + 1}</td>
             <td>{item.appName || item.source}</td>
             <td style={{"wordBreak": "break-word"}}>{item.title}</td>
             <td style={{"wordBreak": "break-word"}}>{item.body}</td>
@@ -41,6 +90,61 @@ class Notifications extends React.PureComponent {
     })
   }
 
+  renderPages(keyPrefix) {
+    let page = this.state.pages;
+    if(page.total > 1) {
+      let items = [];
+      items.push(<Pagination.First key={keyPrefix + "page_first"} />);
+      items.push(<Pagination.Prev key={keyPrefix + "page_prev"} />);
+      items.push(
+        <Pagination.Item key={keyPrefix + "page_1"} active={1 === page.active}>
+          {1}
+        </Pagination.Item>
+      );
+      if(page.active > 5) {
+        items.push(
+          <Pagination.Ellipsis disabled key={keyPrefix + "page_front_dot"} />
+        )
+      } else if(page.active == 5) {
+        items.push(
+          <Pagination.Item key={keyPrefix + "page_2"}>
+            {2}
+          </Pagination.Item>
+        )
+      }
+
+      for (let number = Math.max(2, page.active - 2); number < page.total && number <= page.active + 2; number++) {
+        items.push(
+          <Pagination.Item key={keyPrefix + "page_" + number} active={number === page.active}>
+            {number}
+          </Pagination.Item>
+        );
+      }
+
+      if(page.total - page.active > 4) {
+        items.push(
+          <Pagination.Ellipsis disabled key={keyPrefix + "page_back_dot"} />
+        );
+      } else if(page.total - page.active == 4) {
+        items.push(
+          <Pagination.Item key={keyPrefix + "page_" + (page.total - 1)}>
+            {page.total - 1}
+          </Pagination.Item>
+        );
+      }
+
+      items.push(
+        <Pagination.Item key={keyPrefix + "page_" + page.total} active={page.total === page.active}>
+          {page.total}
+        </Pagination.Item>
+      );
+      items.push(<Pagination.Next key={keyPrefix + "page_next"} />)
+      items.push(<Pagination.Last key={keyPrefix + "page_last"} />)
+
+      return items;
+    }
+  }
+
   render() {
     let headers = [{
       name: "#",
@@ -60,14 +164,28 @@ class Notifications extends React.PureComponent {
     }]
 
     return this.addCSS(
-      <Table striped bordered hover responsive variant="dark">
-        <thead><tr>
-          { this.renderHeaders(headers) }
-        </tr></thead>
-        <tbody id="notifications-table">
-          { this.renderNotifications() }
-        </tbody>
-      </Table>
+      <>
+        <Pagination
+          onClick={this.pageChanged.bind(this)}
+          className="justify-content-center">
+          { this.renderPages("top_") }
+        </Pagination>
+
+        <Table striped bordered hover responsive variant="dark">
+          <thead><tr>
+            { this.renderHeaders(headers) }
+          </tr></thead>
+          <tbody id="notifications-table">
+            { this.renderNotifications() }
+          </tbody>
+        </Table>
+
+        <Pagination
+          onClick={this.pageChanged.bind(this)}
+          className="justify-content-center">
+          { this.renderPages("bottom_") }
+        </Pagination>
+      </>
     )
   }
 
